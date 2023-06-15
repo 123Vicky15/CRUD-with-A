a57e4c42b6a67c406a8a3e939ae1294703117f37
A19A56031 <A19A56031@A19A56031>
2023-06-14 20:28:58 -0400

    Agregar archivos de proyecto.

---
+++ App-API/Controllers/ActorController.cs
@@ -0,0 +1,64 @@
+﻿using App_API.Data;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace App_API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ActorController : ControllerBase
+    {
+        private readonly DataContext _context;
+        public ActorController(DataContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+
+        public async Task<ActionResult<List<Actor>>> GetActor()
+        {
+            return Ok(await _context.actors.ToListAsync());
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<List<Actor>>> CreateActor(Actor actor)
+        {
+            _context.actors.Add(actor);
+            await _context.SaveChangesAsync();
+
+            return Ok(await _context.actors.ToListAsync());
+        }
+
+        [HttpPut]
+        public async Task<ActionResult<List<Actor>>> UpdateActor(Actor actor)
+        {
+            var dbActor = await _context.actors.FindAsync(actor.Id);
+            if (dbActor == null)
+                return BadRequest("Actor not found");
+
+            dbActor.Name = actor.Name;
+            dbActor.FirstName = actor.FirstName;
+            dbActor.LastName = actor.LastName;
+            dbActor.Place = actor.Place;
+
+            await _context.SaveChangesAsync();
+
+            return Ok(await _context.actors.ToArrayAsync());
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<List<Actor>>> DeleteActor(int id)
+        {
+            var dbActor = await _context.actors.FindAsync(id);
+            if (dbActor == null)
+                return BadRequest("Actor not found");
+
+            _context.actors.Remove(dbActor);
+            await _context.SaveChangesAsync();
+
+            return Ok(await _context.actors.ToArrayAsync());
+        }
+    }
+}
---
+++ App-API/Data/DataContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace App_API.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options) { }
+
+        //Representation of the dh, <class>, nameofthetable, set= initialization
+        public DbSet<Actor> actors => Set<Actor>();
+    }
+}
